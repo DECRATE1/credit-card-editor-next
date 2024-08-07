2c46b3020aea47900be145f92662775ffa9e193c
DECRATE1 <yandovin2005@mail.ru>
2024-08-07 12:30:56 +0300

    maybe complete later

--- src/app/components/MakeYourSkinCard/MakeYourSkinCard.tsx
+++ src/app/components/MakeYourSkinCard/MakeYourSkinCard.tsx
@@ -1,32 +1,48 @@
+"use client";
 import Image from "next/image";
 import { useRef } from "react";
 import Moveable from "react-moveable";
-export default function MakeYourSkinCard() {
+
+interface MakeYourSkinCardProps {
+  handleSetIsClicked: (boolean: boolean) => void;
+  isClicked: boolean;
+}
+export default function MakeYourSkinCard({
+  handleSetIsClicked,
+  isClicked,
+}: MakeYourSkinCardProps) {
   const refa = useRef(null);
   return (
     <div className="bg-black text-white w-full">
       <div className="w-full bg-red-600 h-1/4 relative">
-        <div
-          className="w-full h-full bg-[url('/man1.png')] bg-no-repeat bg-cover"
-          ref={refa}
-        ></div>
-        <Moveable
-          target={refa}
-          draggable={true}
-          throttleDrag={1}
-          edgeDraggable={false}
-          startDragRotate={0}
-          throttleDragRotate={0}
-          onDrag={(e) => {
-            e.target.style.transform = e.transform;
-          }}
-          resizable={true}
-          onResize={(e) => {
-            e.target.style.width = `${e.width}px`;
-            e.target.style.height = `${e.height}px`;
-            e.target.style.transform = e.drag.transform;
-          }}
-        />
+        <div className="w-11/12 h-full" ref={refa}>
+          <Image
+            src={"/man1.png"}
+            alt="img"
+            fill
+            onClick={() => handleSetIsClicked(true)}
+          ></Image>
+        </div>
+
+        {isClicked && (
+          <Moveable
+            target={refa}
+            draggable={true}
+            throttleDrag={1}
+            edgeDraggable={false}
+            startDragRotate={0}
+            throttleDragRotate={0}
+            onDrag={(e) => {
+              e.target.style.transform = e.transform;
+            }}
+            resizable={true}
+            onResize={(e) => {
+              e.target.style.width = `${e.width}px`;
+              e.target.style.height = `${e.height}px`;
+              e.target.style.transform = e.drag.transform;
+            }}
+          />
+        )}
       </div>
     </div>
   );
--- src/app/makeYourSkin/page.tsx
+++ src/app/makeYourSkin/page.tsx
@@ -1,17 +1,28 @@
 "use client";
 import Image from "next/image";
 import MakeYourSkinCard from "../components/MakeYourSkinCard/MakeYourSkinCard";
+import { useState } from "react";
 
 export default function MakeYourSkin() {
+  const [isClicked, setIsClicked] = useState(false);
+  const handleSetIsClicked = (boolean: boolean) => {
+    setIsClicked(boolean);
+  };
   return (
     <div className="w-screen h-screen">
       <div className="flex w-screen h-screen">
-        <div className="w-4/5 flex justify-center items-center">
+        <div
+          className="w-4/5 flex justify-center items-center"
+          onClick={() => handleSetIsClicked(false)}
+        >
           <Image src={"/change.png"} alt="img" width={700} height={700}></Image>
         </div>
 
         <div className="flex w-1/5 h-full">
-          <MakeYourSkinCard></MakeYourSkinCard>
+          <MakeYourSkinCard
+            handleSetIsClicked={handleSetIsClicked}
+            isClicked={isClicked}
+          ></MakeYourSkinCard>
         </div>
       </div>
     </div>
